1fe982ea60a75188957cc95903144e17b58e4a6a
Alex Schwartz <dev@schwartzonline.de>
2022-06-01 23:19:08 +0200

    stripped down

--- pet_little_helferlein/__main__.py
+++ pet_little_helferlein/__main__.py
@@ -1,11 +1,11 @@
 """To-Do entry point script."""
 # todo/__main__.py
 
-from todo import cli, __app_name__
+from pet_little_helferlein import cli, __app_name__
 
 
 def main():
-    cli.todo_cli(prog_name=__app_name__)
+    cli.cli(prog_name=__app_name__)
 
 
 if __name__ == "__main__":
